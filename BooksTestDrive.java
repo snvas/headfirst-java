e7c2de0ae6dce386a94a74d477a11b78906cf4be
Soraia Novaes <sonovaes511@gmail.com>
2022-08-07 16:14:57 -0300

    exec chapter primiter and references

---
+++ BooksTestDrive.java
@@ -0,0 +1,27 @@
+class Books{
+	String title;
+	String author;
+}
+
+class BooksTestDrive{
+	public static void main(String[] args){
+		Books[] myBooks = new Books[3];
+		myBooks[0] = new Books();
+		myBooks[1] = new Books();
+		myBooks[2] = new Books();
+		myBooks[0].title = "The Grape of Java";
+		myBooks[1].title = "The Java Gastby";
+		myBooks[2].title = "The Java Cookbook";
+		myBooks[0].author = "bob";
+		myBooks[1].author = "sue";
+		myBooks[2].author = "ian";
+
+		int x = 0;
+		while(x < 3){
+			System.out.print(myBooks[x].title);
+			System.out.print(" by ");
+			System.out.println(myBooks[x].author);
+			x++; 
+		}
+	}
+}
